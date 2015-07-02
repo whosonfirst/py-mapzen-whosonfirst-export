179be982eb9f0faec7e0b98f3a4a3cb70d8431a0
thisisaaronland <aaron@mapzen.com>
2015-07-02 21:19:11 +0000

    stub out a woedb exporter

---
+++ mapzen/gazetteer/export/woedb.py
@@ -0,0 +1,21 @@
+import mapzen.gazetteer.export
+
+class exporter(mapzen.gazetteer.export.flatfile):
+
+    def __init__(self, root, **kwargs):
+
+        mapzen.gazetteer.export.flatfile.__init__(self, root, **kwargs)
+
+class timezone_exporter(exporter):
+
+    def massage_feature(self, f):
+
+        woe_props = f['properties']
+
+        props = {}
+        props['mz:placetype'] = 'timezone'
+        props['mz:source'] = 'woedb'
+        props['mz:name'] = woe_props['name']
+        props['mz:fullname'] woe_props['fullname']
+        props['woe:id'] = woe_props['woe:id']
+        props['woe:source'] = woe_props['provider']
