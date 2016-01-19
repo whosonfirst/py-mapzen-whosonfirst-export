3b341b4a605493a363b8955cd8bfee7de3aafa3b
Dan Phiffer <dan@phiffer.org>
2016-01-19 15:41:07 -0800

    🛠 Added VERSION, requirements.txt

--- setup.py
+++ setup.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.export.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.export',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.export'],
-    version='0.71',
+    version=version,
     description='Simple Python wrapper for managing Who\'s On First export-related functions',
     author='Mapzen',
-    url='https://github.com/thisisaaronland/py-mapzen-whosonfirst-export',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-export',
     install_requires=[
         'requests',
         'geojson',
         'shapely',
         'atomicwrites',
-        'mapzen.whosonfirst.utils>=0.17',
+        'mapzen.whosonfirst.utils>=0.18',
         'mapzen.whosonfirst.geojson>=0.06',
-        'mapzen.whosonfirst.validator>=0.09',
+        'mapzen.whosonfirst.validator>=0.10',
         ],
     dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.17',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.18',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson/tarball/master#egg=mapzen.whosonfirst.geojson-0.06',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-validator/tarball/master#egg=mapzen.whosonfirst.validator-0.09',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-validator/tarball/master#egg=mapzen.whosonfirst.validator-0.10',
         ],
     packages=packages,
     scripts=[
         'scripts/wof-exportify',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-export/releases/tag/v0.71',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-export/releases/tag/' + version,
     license='BSD')
